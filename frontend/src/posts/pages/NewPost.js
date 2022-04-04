eefbda68d81ced7f2d759fb59d5b98c65a117681
haimrubin <73288124+haimrubin@users.noreply.github.com>
2022-04-04 17:55:56 +0300

    BSPM2022T14-71
    add the page to fill all the detail of the post
    add a css too

---
+++ frontend/src/posts/pages/NewPost.js
@@ -0,0 +1,103 @@
+import React, {useContext} from "react";
+import Input from "../../shared/components/FormElements/Input/Input";
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/components/Utils/validators";
+import Button from "../../shared/components/FormElements/Button";
+import {useForm} from "../../shared/hoooks/form-hook";
+import {useHttpClient} from "../../shared/hoooks/http-hook";
+import {AuthContext} from "../../shared/context/auth-context";
+import ErrorModal from "../../shared/components/UIElements/Error/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/Loading/LoadingSpinner";
+import {useHistory} from "react-router-dom";
+import './PostForm.css';
+
+
+const NewPost = () => {
+    const {isLoading, error, sendRequest, clearError} = useHttpClient();
+    const auth = useContext(AuthContext)
+
+    const [formState, inputChangeHandler] = useForm({
+        title: {
+            value: '',
+            isValid: false
+        },
+        description: {
+            value: '',
+            isValid: false
+        },
+        address: {
+            value: '',
+            isValid: false
+        },
+        category: {
+            value: '',
+            isValid: false
+        }
+    },false)
+
+   const history = useHistory();
+
+   const placeSubmitHandler = async event => {
+       event.preventDefault();
+       try {
+           await sendRequest(`http://localhost:5000/api/posts`,
+               'POST',
+               JSON.stringify({
+                   title: formState.inputs.title.value,
+                   description : formState.inputs.description.value,
+                   address : formState.inputs.address.value,
+                   category: formState.inputs.category.value
+               }),
+               {'Content-Type': 'application/json',
+               Authorization: 'Bearer ' + auth.token}
+           )
+        history.push('/'); // redirect
+       }catch (e) {
+           
+       }
+
+   }
+
+    return <React.Fragment>
+        <ErrorModal error={error} onClear={clearError}/>
+        <form className="place-form" onSubmit={placeSubmitHandler}>
+            {isLoading && <LoadingSpinner asOverlay/>}
+            <Input
+                id="title"
+                element='input'
+                type='text'
+                label='Title'
+                validators={[VALIDATOR_REQUIRE()]}
+                onInput={inputChangeHandler}
+                errorText="Please enter a valid title"/>
+
+            <Input
+                id="description"
+                element='textarea'
+                label='Description'
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                onInput={inputChangeHandler}
+                errorText="Please enter a valid description."/>
+
+            <Input
+                id="category"
+                element='textarea'
+                label='Category'
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                onInput={inputChangeHandler}
+                errorText="Please enter a valid category."/>
+
+            <Input
+                id="address"
+                element='input'
+                label='Address'
+                validators={[VALIDATOR_REQUIRE()]}
+                onInput={inputChangeHandler}
+                errorText="Please enter a valid address"/>
+
+            <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
+        </form>
+    </React.Fragment>
+
+};
+
+export default NewPost;
