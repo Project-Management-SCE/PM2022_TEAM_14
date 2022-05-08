7be54dd5f8bcd4f532b717d3ede18aedd3fa9558
Michael <50995165+MikeShveyn@users.noreply.github.com>
2022-05-08 23:59:45 +0300

    jenkins

--- backend/app.test.js
+++ backend/app.test.js
@@ -4,28 +4,21 @@ const server = require('./server')
 
 jest.useFakeTimers('legacy')
 describe("Integration Testing backend and frontend", () => {
-    beforeAll(async () => {
-
-    })
-
-    afterEach(async done => {
+    afterAll(async () => {
         // Closing the DB connection allows Jest to exit successfully.
-        await server.close();
-        done()
+      await server.close();
     });
 
     describe("Testing post routes ", () => {
 
-        test("Update post without permissions should respond with a 403 status code", async (done) => {
+        test("Update post without permissions should respond with a 403 status code", async () => {
             const response = await request(app).patch("/api/posts/625723f2eb32216ea39da867").send({title : 'test', description: 'test'})
             expect(response.statusCode).toBe(403)
-            done()
         })
 
-        test("Update not existing post should respond with a 403 status code", async (done) => {
+        test("Update not existing post should respond with a 403 status code", async () => {
             const response = await request(app).patch("/api/posts/6dsadasd").send({title : 'test', description: 'test'})
             expect(response.statusCode).toBe(403)
-            done()
         })
     })
 
