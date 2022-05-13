7a59741d788ad46e3c03b2c8d8da069109da5abb
Anna Shveynfurt <57673883+AnnaShw@users.noreply.github.com>
2022-05-13 16:16:55 +0300

    BSPM2022T14-129

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,122 +1,126 @@
-import React, {useCallback, useEffect, useState} from 'react';
-import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
-import Auth from "./users/pages/Auth";
-import {AuthContext} from "./shared/context/auth-context";
-import {useAuth} from "./shared/hooks/auth-hook";
-import AllPosts from "./posts/pages/AllPosts";
-import MainNavigation from "./shared/components/navigation/MainNavigation";
-import NewPost from "./posts/pages/NewPost";
-import NewAdminPost from "./posts/pages/NewAdminPost";
-import UpdateAdminPost from "./posts/pages/UpdateAdminPost";
-import Profile from "./users/pages/Profile";
-import UserPosts from "./posts/pages/UserPosts";
-import UpdatePost from "./posts/pages/UpdatePost";
-import UpdateUser from "./users/pages/UpdateUser";
-import UsersList from "./users/components/UsersList";
-import Users from "./users/pages/Users";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-
+const UserPosts = React.lazy(() => import("./posts/pages/UpdatePost"));
+const Auth = React.lazy(() => import("./users/pages/Auth"));
+const AuthContext = React.lazy(() => import("./shared/context/auth-context"));
+const useAuth = React.lazy(() => import("./shared/hooks/auth-hook"));
+const AllPosts = React.lazy(() => import("./posts/pages/AllPosts"));
+const MainNavigation = React.lazy(() =>
+  import("./shared/components/navigation/MainNavigation")
+);
+const NewPost = React.lazy(() => import("./posts/pages/NewPost"));
+const UpdatePost = React.lazy(() => import("./posts/pages/UpdatePost"));
+const NewAdminPost = React.lazy(() => import("./posts/pages/NewAdminPost"));
+const UpdateAdminPost = React.lazy(() =>
+  import("./posts/pages/UpdateAdminPost")
+);
+const Profile = React.lazy(() => import("./users/pages/Profile"));
+const UpdateUser = React.lazy(() => import("./users/pages/UpdateUser"));
 
 const App = () => {
-    const {token, login, logout, userId, isAdmin} = useAuth()
+  const { token, login, logout, userId, isAdmin } = useAuth();
 
-    let routes;
+  let routes;
 
-    if (token && !isAdmin) {
-        routes = (
-            <Switch>
-                <Route path="/" exact={true}>
-                    <AllPosts/>
-                </Route>
+  if (token && !isAdmin) {
+    routes = (
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllPosts />
+        </Route>
 
-                <Route path="/users/:userId">
-                    <Profile/>
-                </Route>
+        <Route path="/users/:userId">
+          <Profile />
+        </Route>
 
-                <Route path="/:userId/posts">
-                    <UserPosts/>
-                </Route>
+        <Route path="/:userId/posts">
+          <UserPosts />
+        </Route>
 
-                <Route path="/posts/new" exact={true}>
-                    <NewPost/>
-                </Route>
+        <Route path="/posts/new" exact={true}>
+          <NewPost />
+        </Route>
 
-                <Route path="/posts/:postId" exact={true}>
-                    <UpdatePost/>
-                </Route>
+        <Route path="/posts/:postId" exact={true}>
+          <UpdatePost />
+        </Route>
 
-                <Route path="/update/:userId">
-                    <UpdateUser/>
-                </Route>
+        <Route path="/update/:userId">
+          <UpdateUser />
+        </Route>
 
-                <Redirect to='/'/>
-            </Switch>
-        );
-    }else if(token && isAdmin) {
-        routes = (
-            <Switch>
-                <Route path="/" exact={true}>
-                    <AllPosts/>
-                </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else if (token && isAdmin) {
+    routes = (
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllPosts />
+        </Route>
 
-                <Route path="/users" exact={true}>
-                    <Users/>
-                </Route>
+        <Route path="/users" exact={true}></Route>
 
-                <Route path="/users/:userId">
-                    <Profile/>
-                </Route>
+        <Route path="/users/:userId">
+          <Profile />
+        </Route>
 
-                <Route path="/:userId/posts">
-                    <UserPosts/>
-                </Route>
+        <Route path="/:userId/posts">
+          <UserPosts />
+        </Route>
 
-                <Route path="/posts/new" exact={true}>
-                    <NewAdminPost/>
-                </Route>
+        <Route path="/posts/new" exact={true}>
+          <NewAdminPost />
+        </Route>
 
-                <Route path="/posts/:postId" exact={true}>
-                    <UpdateAdminPost/>
-                </Route>
+        <Route path="/posts/:postId" exact={true}>
+          <UpdateAdminPost />
+        </Route>
 
-                <Redirect to='/'/>
-            </Switch>
-        );
-    } else {
-        routes = (
-            <Switch>
-                <Route path="/" exact={true}>
-                    <AllPosts/>
-                </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllPosts />
+        </Route>
 
-                <Route path="/:userId/posts">
+        <Route path="/:userId/posts"></Route>
 
-                </Route>
+        <Route path="/auth" exact={true}>
+          <Auth />
+        </Route>
 
-                <Route path="/auth" exact={true}>
-                    <Auth/>
-                </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
 
-                <Redirect to='/auth'/>
-            </Switch>
-        );
-    }
-
-
-    return <AuthContext.Provider value={{
+  return (
+    <AuthContext.Provider
+      value={{
         isLoggedIn: !!token,
         token: token,
         userId,
         isAdmin,
         login,
-        logout
-    }}>
-        <Router>
-            <MainNavigation/>
-            <main>{routes}</main>
-        </Router>
+        logout,
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
     </AuthContext.Provider>
-
+  );
 };
 
 export default App;
