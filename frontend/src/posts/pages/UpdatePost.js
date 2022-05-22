52cb078ad4d022a4aca00ebd7a26d535decf835a
haimrubin <73288124+haimrubin@users.noreply.github.com>
2022-05-22 12:58:17 +0300

    BSPM2022T14-111
    add a delete button of image from a post

--- frontend/src/posts/pages/UpdatePost.js
+++ frontend/src/posts/pages/UpdatePost.js
@@ -27,7 +27,11 @@ const UpdatePost = () => {
         description: {
             value: '',
             isValid: true
-        }
+        },
+        image: {
+            value: '',
+            isValid: false
+        },
     }, true)
 
     const placeUpdateSubmitHandler = async event => {
@@ -38,6 +42,7 @@ const UpdatePost = () => {
                 JSON.stringify({
                     title: formState.inputs.title.value,
                     description : formState.inputs.description.value,
+                    image: formState.inputs.image.value,
                 }),
                 {'Content-Type': 'application/json',
                 Authorization: 'Bearer ' + auth.token}
@@ -115,6 +120,15 @@ const UpdatePost = () => {
                 initialValue={loadedPost.description}
                 initialIsValid={true}
                 errorText="Please enter a valid description."/>
+
+            <Input
+                id="image"
+                element='input'
+                label='Image'
+                validators={[]}
+                onInput={inputHandler}
+                initialValue={loadedPost.image}
+                errorText="Please enter a url"/>
 
             <Button type="submit" disabled={!formState.isValid}>UPDATE PLACE</Button>
         </form>}
